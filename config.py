6cb3f963d0d4f28c03f349f699a1f7d13696fbbb
VoidOG <VoidGod123@outlook.com>
2025-07-02 18:39:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
-SESSION_NAME = "BQGz0ToAuN4b64CXcaxw2Z9NXSrv4NlOlKHCoUZaFsC5oLcCARqEisXl8acO4mcve-xIm92XwSZts6Jhzwi-65hfU1RZCU9NKTLL5ILsMUrNSwqBCFVbKET6ObyuZ2j2MRWWXlMyPR8h9JPyGVGDQbuKuzL9Dz4Cgip00kdAYDaM96DleIqVj2GegNmn6TYjEH9nfVrcfOrIGDukEYKekcBVFvn8NlnY0r_lunpP7O0CvrPN4hpOptGUFW2gzeOPfjS_L-2JnKUFluHSMqCvR3WeE5KnFptbN2n3qeN_z8I5ypcQ26phHKkdpx4tPcF96q2Xjvd9OrRxGRRLJqjRKLM6LEh7NQAAAAHPmT5cAA"
+# config.py
 
-# Extract from the link: https://t.me/YourChannel/42
-SOURCE_CHANNEL_USERNAME = "adversia"  # without @
-MESSAGE_ID = 2
+API_ID = 28561722  # your API ID
+API_HASH = "a538ba07656f746def99bed7032121cc"  # your API Hash
+
+SESSION_STRING = "BQGz0ToAuN4b64CXcaxw2Z9NXSrv4NlOlKHCoUZaFsC5oLcCARqEisXl8acO4mcve-xIm92XwSZts6Jhzwi-65hfU1RZCU9NKTLL5ILsMUrNSwqBCFVbKET6ObyuZ2j2MRWWXlMyPR8h9JPyGVGDQbuKuzL9Dz4Cgip00kdAYDaM96DleIqVj2GegNmn6TYjEH9nfVrcfOrIGDukEYKekcBVFvn8NlnY0r_lunpP7O0CvrPN4hpOptGUFW2gzeOPfjS_L-2JnKUFluHSMqCvR3WeE5KnFptbN2n3qeN_z8I5ypcQ26phHKkdpx4tPcF96q2Xjvd9OrRxGRRLJqjRKLM6LEh7NQAAAAHPmT5cAA"  # your full Pyrogram string session
+
+SOURCE_CHANNEL_LINK = "https://t.me/adversia/2"  # message link
+DELAY_SECONDS = 30  # 1 hour delay
+
+LOG_GROUP_ID = -1002854086015  # your log group/channel
 
 FORUM_TARGETS = [
     {"chat_id": -1002063425506, "topic_id": 823},
@@ -11,7 +17,3 @@ FORUM_TARGETS = [
 NORMAL_GROUPS = [
     -1002529323673,
 ]
-
-DELAY_SECONDS = 30  # seconds
-
-LOG_GROUP_ID = -1002854086015  # log group/channel
