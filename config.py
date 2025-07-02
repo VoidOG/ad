03a32abc427596afd73993b910e7e7d2fe52e106
VoidOG <VoidGod123@outlook.com>
2025-07-02 18:00:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+SESSION_STRING = "your_pyrogram_string_here"
+
+SOURCE_CHANNEL_ID = -1001234567890  # Source channel
+MESSAGE_ID = 42  # Message to forward
+
+FORUM_TARGETS = [
+    {"chat_id": -1001122334455, "topic_id": 1234},
+    {"chat_id": -1006677889900, "topic_id": 5678},
+]
+
+NORMAL_GROUPS = [
+    -1009988776655,
+    -1002233445566,
+]
+
+DELAY_SECONDS = 3600  # 1 hour
